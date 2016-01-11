41dd7af89504ccee7c5e5c639ed0ca3ccf7fbb10
ArcGhost <alex@tassl.com>
2016-01-11 15:48:49 -0800

    added Debug into __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['DEBUG'] = True #need this to see shit
 db = SQLAlchemy(app)
 
 from app import views, models
