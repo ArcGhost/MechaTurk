ffbe9f94e7b7319534697c52df30321633b365b4
ArcGhost <alex@tassl.com>
2016-01-18 21:07:35 +0800

    needed to fix dualsubmit.js and still need to fix the conditional submit button in the template

--- app/static/dualsubmit.js
+++ app/static/dualsubmit.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
-
     $(":submit").click(function(e) {
         e.preventDefault();
-        console.log('First, the data is posted to OUR server.');
+        // console.log('First, the data is posted to OUR server.');
         // submit to our server
         $.ajax({
-            url: window.location.href,
+            url: window.location.href, //this should be the internal iframe url
             type: 'post',
             data: $('form').serialize(),
             success: function(result) { 
                 console.log('Then, the data is posted to AWS server.');
                 $('form').submit();// submit to mechanical turk
             }
-
 
         });
 });
