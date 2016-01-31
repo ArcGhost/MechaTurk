15dff353bc71ce668531414cea09567eb852a065
ArcGhost <alex@tassl.com>
2016-01-31 13:51:23 -0800

    switched order of vars in User model password-hash check to test

--- app/models.py
+++ app/models.py
@@ -30,7 +30,8 @@ class User(db.Model):
         self._password = bcrypt.generate_password_hash(plaintext)
 
     def is_correct_password(self, plaintext):
-        return bcrypt.check_password_hash(self._password, plaintext)
+        #return bcrypt.check_password_hash(self._password, plaintext)
+        return bcrypt.check_password_hash(plaintext, self._password)
 
     @property
     def is_authenticated(self):
